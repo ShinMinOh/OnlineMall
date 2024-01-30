469eeac23ff9399aff134d2be61c4cae6170f72d
ShinMinOh <luciano97@naver.com>
2024-01-30 12:53:09 +0900

    [Test] User 엔티티 테스트 코드 수정

--- src/test/java/com/shop/onlinemall/user/UserTest.java
+++ src/test/java/com/shop/onlinemall/user/UserTest.java
@@ -2,6 +2,11 @@ package com.shop.onlinemall.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -13,11 +18,10 @@ class UserTest {
   @Test
   void User_엔티티_생성(){
     User user = User.builder().id(1L)
-        .address("address")
         .email("email")
         .name("name")
         .password("1111")
-        .role(Role.USER).build();
+        .role(Role.ROLE_USER).build();
 
     Assertions.assertThat(user).isNotNull();
   }
